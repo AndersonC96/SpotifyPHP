09ebe91ab215dccf65ce73f29a6f6b09fb981f2f
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-08-02 15:43:47 -0300

    Create config.php

---
+++ src/config.php
@@ -0,0 +1,5 @@
+<?php
+    define('CLIENT_ID', 'your-client-id');
+    define('CLIENT_SECRET', 'your-client-secret');
+    define('REDIRECT_URI', 'http://localhost/SpotifyPHP/public/callback.php');
+?>
