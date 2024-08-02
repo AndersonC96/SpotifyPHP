1f2ccc28ea4ce5f3414e7eede34ddd0658970a96
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-08-02 18:00:51 -0300

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,5 +1,11 @@
 <?php
-    define('CLIENT_ID', 'your-client-id');
-    define('CLIENT_SECRET', 'your-client-secret');
-    define('REDIRECT_URI', 'http://localhost/SpotifyPHP/public/callback.php');
+    if(!defined('CLIENT_ID')){
+        define('CLIENT_ID', 'your-client-id');
+    }
+    if(!defined('CLIENT_SECRET')){
+        define('CLIENT_SECRET', 'your-client-secret');
+    }
+    if(!defined('REDIRECT_URI')){
+        define('REDIRECT_URI', 'http://localhost/SpotifyPHP/public/callback.php');
+    }
 ?>
