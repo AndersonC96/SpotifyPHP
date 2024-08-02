f4e848fccdaac0f11be25b8e2b93744eb7f7a1ac
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-08-02 17:13:51 -0300

    Update callback.php

--- public/callback.php
+++ public/callback.php
@@ -6,8 +6,17 @@
     if(isset($_GET['code'])){
         $code = $_GET['code'];
         $token = SpotifyAuth::getAccessToken($code);
-        $_SESSION['access_token'] = $token['access_token'];
-        header('Location: dashboard.php');
-        exit();
+        if(isset($token['access_token'])){
+            $_SESSION['access_token'] = $token['access_token'];
+            header('Location: dashboard.php');
+            exit();
+        }else{
+            echo "Failed to get access token.";
+            echo '<pre>';
+            print_r($token);
+            echo '</pre>';
+        }
+    }else{
+        echo "No code parameter in URL.";
     }
 ?>
