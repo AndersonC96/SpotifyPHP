16c0124aac943ac2d9bdc437c00e217442045e56
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-08-05 10:56:58 -0300

    Update podcasts.php

--- public/podcasts.php
+++ public/podcasts.php
@@ -1,52 +1,47 @@
 <?php
-require '../vendor/autoload.php';
-require '../src/SpotifyAuth.php';
-require '../src/SpotifyAPI.php';
-require '../src/config.php';
-
-session_start();
-
-if (!isset($_SESSION['access_token'])) {
-    header('Location: index.php');
-    exit();
-}
-
-$accessToken = $_SESSION['access_token'];
-$spotify = new SpotifyAPI($accessToken);
-$podcasts = $spotify->getUserPodcasts();
+    require '../vendor/autoload.php';
+    require '../src/SpotifyAuth.php';
+    require '../src/SpotifyAPI.php';
+    require '../src/config.php';
+    session_start();
+    if(!isset($_SESSION['access_token'])){
+        header('Location: index.php');
+        exit();
+    }
+    $accessToken = $_SESSION['access_token'];
+    $spotify = new SpotifyAPI($accessToken);
+    $podcasts = $spotify->getUserPodcasts();
 ?>
-
-<?php include 'navbar.php'; ?>
-        <div class="card">
-            <h2 class="text-2xl font-bold mb-4">Recent Podcast Episodes</h2>
-            <?php if (isset($podcasts['items']) && is_array($podcasts['items'])): ?>
+            <?php include 'navbar.php'; ?>
+            <div class="card">
+                <h2 class="text-2xl font-bold mb-4">Recent Podcast Episodes</h2>
+                <?php if (isset($podcasts['items']) && is_array($podcasts['items'])): ?>
                 <?php foreach ($podcasts['items'] as $show): ?>
-                    <?php
-                    // Get the first episode of the show
+                <?php
                     $episodes = $spotify->callAPI("https://api.spotify.com/v1/shows/{$show['show']['id']}/episodes?limit=1");
-                    if (isset($episodes['items'][0])) {
+                    if(isset($episodes['items'][0])){
                         $episode = $episodes['items'][0];
-                    } else {
+                    }else{
                         continue;
                     }
-                    ?>
-                    <div class="episode">
-                        <div class="flex items-center">
-                            <?php if (isset($episode['images'][0]['url'])): ?>
-                                <img src="<?= htmlspecialchars($episode['images'][0]['url']) ?>" alt="Episode Image" width="50">
-                            <?php endif; ?>
-                            <div class="ml-4">
-                                <p class="font-bold"><?= htmlspecialchars($episode['name'] ?? 'No name') ?></p>
-                                <p class="text-sm"><?= htmlspecialchars($episode['description'] ?? 'No description') ?></p>
-                            </div>
+                ?>
+                <div class="episode">
+                    <div class="flex items-center">
+                        <?php if (isset($episode['images'][0]['url'])): ?>
+                        <img src="<?= htmlspecialchars($episode['images'][0]['url']) ?>" alt="Episode Image" width="50">
+                        <?php endif; ?>
+                        <div class="ml-4">
+                            <p class="font-bold"><?= htmlspecialchars($episode['name'] ?? 'No name') ?></p>
+                            <p class="text-sm"><?= htmlspecialchars($episode['description'] ?? 'No description') ?></p>
                         </div>
-                        <a href="<?= htmlspecialchars($episode['external_urls']['spotify'] ?? '#') ?>" target="_blank" class="view-button">Listen</a>
                     </div>
+                    <a href="<?= htmlspecialchars($episode['external_urls']['spotify'] ?? '#') ?>" target="_blank" class="view-button">Listen</a>
+                </div>
                 <?php endforeach; ?>
-            <?php else: ?>
+                <?php else: ?>
                 <p>No recent episodes found.</p>
-            <?php endif; ?>
+                <?php endif; ?>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
