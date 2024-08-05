22d60a89f66053c74c8eb7b8b0a718c859424b61
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-08-05 18:05:57 -0300

    Update SpotifyAPI.php

--- src/SpotifyAPI.php
+++ src/SpotifyAPI.php
@@ -4,13 +4,18 @@
         public function __construct($accessToken){
             $this->accessToken = $accessToken;
         }
-        public function callAPI($url){
+        private function callAPI($url, $method = 'GET', $data = null){
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
             curl_setopt($ch, CURLOPT_HTTPHEADER, [
                 'Authorization: Bearer ' . $this->accessToken,
+                'Content-Type: application/json',
             ]);
+            if($data){
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            }
             $result = curl_exec($ch);
             if(curl_errno($ch)){
                 echo 'Error:' . curl_error($ch);
@@ -40,22 +45,56 @@
             return $this->callAPI('https://api.spotify.com/v1/me/player/currently-playing');
         }
         public function playTrack($trackUri){
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'https://api.spotify.com/v1/me/player/play');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_PUT, 1);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, [
-                'Authorization: Bearer ' . $this->accessToken,
-                'Content-Type: application/json',
-            ]);
             $data = json_encode(['uris' => [$trackUri]]);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-            $result = curl_exec($ch);
-            if(curl_errno($ch)){
-                echo 'Error:' . curl_error($ch);
-            }
-            curl_close($ch);
-            return json_decode($result, true);
+            return $this->callAPI('https://api.spotify.com/v1/me/player/play', 'PUT', $data);
+        }
+        public function getTopTracks(){
+            return $this->callAPI('https://api.spotify.com/v1/me/top/tracks?limit=10');
+        }
+        public function getTopArtists(){
+            return $this->callAPI('https://api.spotify.com/v1/me/top/artists?limit=10');
+        }
+        public function getPlaybackState(){
+            return $this->callAPI('https://api.spotify.com/v1/me/player');
+        }
+        public function transferPlayback($deviceIds){
+            return $this->callAPI('https://api.spotify.com/v1/me/player', 'PUT', ['device_ids' => $deviceIds]);
+        }
+        public function getAvailableDevices(){
+            return $this->callAPI('https://api.spotify.com/v1/me/player/devices');
+        }
+        public function startPlayback($deviceId = null, $contextUri = null, $uris = null){
+            $data = [];
+            if($deviceId) $data['device_id'] = $deviceId;
+            if($contextUri) $data['context_uri'] = $contextUri;
+            if($uris) $data['uris'] = $uris;
+            return $this->callAPI('https://api.spotify.com/v1/me/player/play', 'PUT', $data);
+        }
+        public function pausePlayback(){
+            return $this->callAPI('https://api.spotify.com/v1/me/player/pause', 'PUT');
+        }
+        public function skipToNext(){
+            return $this->callAPI('https://api.spotify.com/v1/me/player/next', 'POST');
+        }
+        public function skipToPrevious(){
+            return $this->callAPI('https://api.spotify.com/v1/me/player/previous', 'POST');
+        }
+        public function seekToPosition($positionMs){
+            return $this->callAPI('https://api.spotify.com/v1/me/player/seek?position_ms=' . $positionMs, 'PUT');
+        }
+        public function setRepeatMode($state){
+            return $this->callAPI('https://api.spotify.com/v1/me/player/repeat?state=' . $state, 'PUT');
+        }
+        public function setPlaybackVolume($volumePercent){
+            return $this->callAPI('https://api.spotify.com/v1/me/player/volume?volume_percent=' . $volumePercent, 'PUT');
+        }
+        public function togglePlaybackShuffle($state){
+            return $this->callAPI('https://api.spotify.com/v1/me/player/shuffle?state=' . $state, 'PUT');
+        }
+        public function getUsersQueue(){
+            return $this->callAPI('https://api.spotify.com/v1/me/player/queue');
+        }
+        public function addItemToQueue($uri){
+            return $this->callAPI('https://api.spotify.com/v1/me/player/queue?uri=' . $uri, 'POST');
         }
     }
-?>
