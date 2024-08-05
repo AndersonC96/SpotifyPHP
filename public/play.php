921409b29d09b9eabfbe488ae48a8f32205500e7
Anderson Cavalcante Barbosa <8729281+AndersonC96@users.noreply.github.com>
2024-08-05 11:37:53 -0300

    Update play.php

--- public/play.php
+++ public/play.php
@@ -5,17 +5,17 @@
     require '../src/config.php';
     session_start();
     if(!isset($_SESSION['access_token'])){
-        echo json_encode(['success' => false]);
+        echo json_encode(['success' => false, 'message' => 'No access token']);
         exit();
     }
     $accessToken = $_SESSION['access_token'];
     $spotify = new SpotifyAPI($accessToken);
     $data = json_decode(file_get_contents('php://input'), true);
-    $trackUri = $data['uri'];
-    $response = $spotify->playTrack($trackUri);
-    if($response){
-        echo json_encode(['success' => true]);
+    $trackUri = $data['uri'] ?? '';
+    if($trackUri){
+        $result = $spotify->playTrack($trackUri);
+        echo json_encode(['success' => true, 'result' => $result]);
     }else{
-        echo json_encode(['success' => false]);
+        echo json_encode(['success' => false, 'message' => 'No track URI provided']);
     }
 ?>
